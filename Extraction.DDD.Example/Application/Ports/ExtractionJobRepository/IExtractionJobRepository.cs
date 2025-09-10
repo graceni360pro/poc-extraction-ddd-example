c9a92188982e24f7701587574141ccdca5da8fc2
graceni360pro <graceni360pro@gmail.com>
2025-09-10 16:40:51 -0400

    add comments about the purpose of a repository

--- Extraction.DDD.Example/Application/Ports/ExtractionJobRepository/IExtractionJobRepository.cs
+++ Extraction.DDD.Example/Application/Ports/ExtractionJobRepository/IExtractionJobRepository.cs
@@ -1,5 +1,10 @@
 ﻿namespace Extraction.DDD.Example.Application.Ports.ExtractionJobRepository
 {
+	/// <summary>
+	/// The repository's purpose is to abstract persistence for the domain model. It stores and retrieves domain objects.
+	/// It knows how to re-hydrate domain objects from the persistence model and how to persist domain objects.
+	/// It should not receive or return DTOs, only domain objects.
+	/// </summary>
 	public interface IExtractionJobRepository
 	{
 		public StoreExtractionJobResponseDTO StoreExtractionJob(ExtractionJob extractionJob);
