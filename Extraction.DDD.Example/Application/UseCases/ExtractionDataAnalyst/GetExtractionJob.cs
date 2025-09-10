a1a566773577ad799256559544e6efc737686ead
graceni360pro <graceni360pro@gmail.com>
2025-09-10 11:11:37 -0400

    Add explanation for adapting

--- Extraction.DDD.Example/Application/UseCases/ExtractionDataAnalyst/GetExtractionJob.cs
+++ Extraction.DDD.Example/Application/UseCases/ExtractionDataAnalyst/GetExtractionJob.cs
@@ -16,7 +16,9 @@ namespace Extraction.DDD.Example.Application.Actors.ExtractionDataAnalyst
 			GetExtractionJobRepositoryRequestDTO repositoryGetExtractionJobRequestDTO = new GetExtractionJobRepositoryRequestDTO();
 			ExtractionJob extractionJob = extractionJobRepository.GetExtractionJob(repositoryGetExtractionJobRequestDTO);
 
-			// Adapt to an output DTO for the use case. The domain model should not be exposed to the clients of the Application Layer.
+			// Adapt to an output DTO for the use case.
+			// The ExtractionJob is an Application model (since it's an implementation detail of the use-case), but since it contains a domain model (ExtractedField), it must be adapted.
+			// Domain models should not be exposed to the clients of the Application Layer so that we can evolve the domain model more easily without necessarily impacting the clients of the Application Layer.
 			GetExtractDocumentResponseDTO response = GetExtractedDocumentRsponseDTOMapper.ToDto(extractionJob);
 			return response;
 		}
