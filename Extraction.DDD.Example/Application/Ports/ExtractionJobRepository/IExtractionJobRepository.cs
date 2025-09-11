8c31221c7e132f7fb8f302e48d1cddf3abc967f4
graceni360pro <graceni360pro@gmail.com>
2025-09-11 10:59:34 -0400

    update note on the repository

--- Extraction.DDD.Example/Application/Ports/ExtractionJobRepository/IExtractionJobRepository.cs
+++ Extraction.DDD.Example/Application/Ports/ExtractionJobRepository/IExtractionJobRepository.cs
@@ -4,9 +4,10 @@
 	/// The repository's purpose is to abstract persistence for a model. It should provide the illusion of an in-memory collection.
 	/// It knows how to re-hydrate objects from the persistence model, and how to persist objects.
 	/// Rules:
-	/// 1. It should not post or retrieve DTOs.
-	/// 2. If the type of object stored/retrieved is a domain object, the repository should ideally should not be storing the subcomponents of the domain object separately. This risks breaking the integrity of the domain object (like rules that should apply to the whole object, not just parts of it).
-	/// 
+	/// 1. It should not post or retrieve DTOs. Otherwise this forces the consumer to re-hydrate the real model which scatters this logic around the codebase instead of centralizing it in the repository.
+	/// 2. A repository should only manage one aggregate root (a root entity and its children). It should not manage multiple aggregate roots.
+	/// 3. An implementation of a repository may use multiple tables/collections. However, the closer the persistence model is to the domain model, the better, so that it is less confusing.
+	/// --------------------------------------------------
 	/// Here we're storing the whole Extraction Job, which is an application-layer-specific object (not a domain object) that also contains a domain object.
 	/// Ideally we should be able to swap out the application layer's implementation (using a Job pattern) without affecting how the domain object is stored.
 	/// We can refactor this later to store the extraction results separate from the extraction job, but still leave users of this repository unaffected.
